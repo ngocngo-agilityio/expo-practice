3ce214ceb24988eca2733db1678ab145d4515c2c
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2025-11-23 22:45:22 +0700

    feat: write unit test for PhoneNumberInput

--- components/PhoneNumberInput/__test__/PhoneNumberInput.test.tsx
+++ components/PhoneNumberInput/__test__/PhoneNumberInput.test.tsx
@@ -1,53 +1,21 @@
 // Libs
-import { fireEvent, render } from '@/test-utils';
+import { render } from '@/test-utils';
 import React from 'react';
 
 // Components
 import PhoneNumberInput from '..';
 
 // Constants
-import { COUNTRY_CODE } from '@/constants';
 
 const mockHandleChange = jest.fn();
+const mockValue = '0364895651';
 
 describe('PhoneNumberInput Component', () => {
   it('to match snapshot', () => {
-    const { toJSON } = render(<PhoneNumberInput />);
+    const { toJSON } = render(
+      <PhoneNumberInput onChangeText={mockHandleChange} value={mockValue} />,
+    );
 
     expect(toJSON()).toMatchSnapshot();
-  });
-
-  it('should allow typing digits after the country code', () => {
-    const { getByDisplayValue, getByLabelText } = render(
-      <PhoneNumberInput onChangeText={mockHandleChange} />,
-    );
-
-    const input = getByLabelText('Enter your Phone Number');
-    fireEvent.changeText(input, `${COUNTRY_CODE}123`);
-
-    expect(getByDisplayValue(`${COUNTRY_CODE}123`)).toBeTruthy();
-    expect(mockHandleChange).toHaveBeenCalledWith(`${COUNTRY_CODE}123`);
-  });
-
-  it('should not allow deleting country code', () => {
-    const { getByDisplayValue, getByLabelText } = render(<PhoneNumberInput />);
-    const input = getByLabelText('Enter your Phone Number');
-
-    fireEvent.changeText(input, '');
-
-    expect(getByDisplayValue(COUNTRY_CODE)).toBeTruthy();
-  });
-
-  it('should reset to country code when user deletes all digits', () => {
-    const { getByLabelText } = render(
-      <PhoneNumberInput onChangeText={mockHandleChange} />,
-    );
-
-    const input = getByLabelText('Enter your Phone Number');
-
-    fireEvent.changeText(input, `${COUNTRY_CODE}123456`);
-    fireEvent.changeText(input, `${COUNTRY_CODE}`);
-
-    expect(mockHandleChange).toHaveBeenLastCalledWith(COUNTRY_CODE);
   });
 });
